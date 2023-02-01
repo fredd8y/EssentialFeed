474aab9cf4d1b882da6b4345c6424c0dd63ab18a
Federico Arvat <arvat.federico@gmail.com>
2023-02-01 23:16:35 +0100

    Created the FeedItem module

---
+++ EssentialFeed/Feedtem.swift
@@ -0,0 +1,15 @@
+//
+//  Feedtem.swift
+//  EssentialFeed
+//
+//  Created by Federico Arvat on 01/02/23.
+//
+
+import Foundation
+
+struct FeedItem {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let imagE: URL
+}
