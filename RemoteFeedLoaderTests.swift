d8b5c4a5cbd408214e77f12b5e3e5f0649d7ae31
Federico Arvat <arvat.federico@gmail.com>
2023-02-02 22:02:50 +0100

    Combined spy captured params into a single type (tuple)

--- RemoteFeedLoaderTests.swift
+++ RemoteFeedLoaderTests.swift
@@ -5,8 +5,8 @@
 //  Created by Federico Arvat on 02/02/23.
 //
 
-import XCTest
 import EssentialFeed
+import XCTest
 
 // MARK: - RemoteFeedLoaderTests
 
@@ -27,45 +27,51 @@ final class RemoteFeedLoaderTests: XCTestCase {
 
 		XCTAssertEqual(client.requestedURLs, [url])
 	}
-	
+
 	func test_loadTwice_requestsDataFromURLTwice() {
 		let url = URL(string: "https://a-given-url.com")!
 		let (sut, client) = makeSUT(url: url)
-		
+
 		sut.load()
 		sut.load()
-		
+
 		XCTAssertEqual(client.requestedURLs, [url, url])
 	}
-	
+
 	func test_load_deliversErrorOnClientError() {
 		let (sut, client) = makeSUT()
-		
+
 		var capturedErrors = [RemoteFeedLoader.Error]()
-		
-		sut.load() { capturedErrors.append($0) }
-		
+
+		sut.load { capturedErrors.append($0) }
+
 		let clientError = NSError(domain: "Test", code: 0)
-		
+
 		client.complete(with: clientError)
-		
+
 		XCTAssertEqual(capturedErrors, [.connectivity])
 	}
 
 	// MARK: Private
 
 	private class HTTPClientSpy: HTTPClient {
+		// MARK: Internal
+
+		var requestedURLs: [URL] {
+			return messages.map { $0.url }
+		}
+
 		func get(from url: URL, completion: @escaping (Error) -> Void) {
-			completions.append(completion)
-			requestedURLs.append(url)
+			messages.append((url, completion))
 		}
-		
+
 		func complete(with error: Error, at index: Int = 0) {
-			completions[index](error)
+			messages[index].completion(error)
 		}
-		
-		var requestedURLs = [URL]()
-		var completions = [(Error) -> Void]()
+
+		// MARK: Private
+
+		private var messages = [(url: URL, completion: (Error) -> Void)]()
 	}
 
 	// MARK: - Helpers
