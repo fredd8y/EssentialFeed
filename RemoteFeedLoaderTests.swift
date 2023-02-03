31f39eb93533e69d0e742431114f5abda5af558c
Federico Arvat <arvat.federico@gmail.com>
2023-02-03 18:56:42 +0100

    Use explicit JSON Data for non 200 HTTP Response tests

--- RemoteFeedLoaderTests.swift
+++ RemoteFeedLoaderTests.swift
@@ -51,10 +51,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
 		let (sut, client) = makeSUT()
 
 		let samples = [199, 201, 300, 400, 500]
-
+		let json = makeItemJSON([])
 		samples.enumerated().forEach { index, code in
 			expect(sut, toCompleteWith: .failure(.invalidData)) {
-				client.complete(withStatusCode: code, at: index)
+				client.complete(withStatusCode: code, data: json, at: index)
 			}
 		}
 	}
@@ -111,7 +111,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
 			messages[index].completion(.failure(error))
 		}
 
-		func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
+		func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
 			let response = HTTPURLResponse(
 				url: requestedURLs[index],
 				statusCode: code,
