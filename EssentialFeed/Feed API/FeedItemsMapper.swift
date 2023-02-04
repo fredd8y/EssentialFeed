cbda9731a4daa9f0d21e41819d9034fdb65f0fef
Federico Arvat <arvat.federico@gmail.com>
2023-02-04 16:21:49 +0100

    Removed the generic error type from the loadFeedResult to somplify type constraint

--- EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -19,7 +19,7 @@ internal final class FeedItemsMapper {
 			response.statusCode == OK_200,
 			let root = try? JSONDecoder().decode(Root.self, from: data)
 		else {
-			return .failure(.invalidData)
+			return .failure(RemoteFeedLoader.Error.invalidData)
 		}
 		
 		let items = root.feed
--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -24,7 +24,7 @@ public final class RemoteFeedLoader: FeedLoader {
 		case invalidData
 	}
 
-	public typealias Result = LoadFeedResult<Error>
+	public typealias Result = LoadFeedResult
 
 	public func load(completion: @escaping (Result) -> Void) {
 		client.get(from: url) { [weak self] result in
@@ -34,7 +34,7 @@ public final class RemoteFeedLoader: FeedLoader {
 			case let .success(data, response):
 				completion(FeedItemsMapper.map(data, from: response))
 			case .failure:
-				completion(.failure(.connectivity))
+				completion(.failure(Error.connectivity))
 			}
 		}
 	}
