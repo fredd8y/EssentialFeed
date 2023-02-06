9c54b28b160c943051c729851f90093a597f09b3
Federico Arvat <arvat.federico@gmail.com>
2023-02-06 21:17:04 +0100

    moved URL creation to a shared factory method

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -50,7 +50,7 @@ class URLSessionHTTPClientTests: XCTestCase {
 	}
 
 	func test_getFromURL_performsGETRequestWithURL() {
-		let url = URL(string: "http://any-url.com")!
+		let url = anyURL()
 		let exp = expectation(description: "Wait for request")
 
 		URLProtocolStub.observeRequest { request in
@@ -66,13 +66,12 @@ class URLSessionHTTPClientTests: XCTestCase {
 	}
 
 	func test_getFromURL_failsOnRequestError() {
-		let url = URL(string: "http://any-url.com")!
 		let error = NSError(domain: "any error", code: 1)
 		URLProtocolStub.stub(data: nil, response: nil, error: error)
 
 		let exp = expectation(description: "Wait for completion")
 
-		makeSUT().get(from: url) { result in
+		makeSUT().get(from: anyURL()) { result in
 			switch result {
 			case let .failure(receivedError as NSError):
 				XCTAssertEqual(error.domain, receivedError.domain)
@@ -146,6 +145,10 @@ class URLSessionHTTPClientTests: XCTestCase {
 		private static var requestObserver: ((URLRequest) -> Void)?
 
 		private static var stub: Stub?
+	}
+	
+	private func anyURL() -> URL {
+		return URL(string: "http://any-url.com")!
 	}
 
 	private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> URLSessionHTTPClient {
