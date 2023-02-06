c28b33274f4e8183067aa0fd66032b3cc8a6664e
Federico Arvat <arvat.federico@gmail.com>
2023-02-06 20:48:03 +0100

    removed wrong userInfo initialisation

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -40,7 +40,7 @@ class URLSessionHTTPClientTests: XCTestCase {
 	func test_getFromURL_failsOnRequestError() {
 		URLProtocolStub.startInterceptingRequests()
 		let url = URL(string: "http://any-url.com")!
-		let error = NSError(domain: "any error", code: 1, userInfo: nil)
+		let error = NSError(domain: "any error", code: 1)
 		URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
 
 		let sut = URLSessionHTTPClient()
