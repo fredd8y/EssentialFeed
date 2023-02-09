4dbc4090d03a241eb9159f985e1e6fb70760bf87
Federico Arvat <arvat.federico@gmail.com>
2023-02-09 19:24:17 +0100

    save command request cache insertion with timestamp on successful deletion

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -5,107 +5,148 @@
 //  Created by Federico Arvat on 09/02/23.
 //
 
-import XCTest
 import EssentialFeed
+import XCTest
+
+// MARK: - LocalFeedLoader
 
 class LocalFeedLoader {
-	private let store: FeedStore
-	
-	init(store: FeedStore) {
+	// MARK: Lifecycle
+
+	init(store: FeedStore, currentDate: @escaping () -> Date) {
 		self.store = store
+		self.currentDate = currentDate
 	}
-	
+
+	// MARK: Internal
+
 	func save(_ items: [FeedItem]) {
 		store.deleteCacheFeed { [unowned self] error in
 			if error == nil {
-				self.store.insert(items)
+				self.store.insert(items, timestamp: currentDate())
 			}
 		}
 	}
+
+	// MARK: Private
+
+	private let store: FeedStore
+	
+	private let currentDate: () -> Date
 }
 
+// MARK: - FeedStore
+
 class FeedStore {
+	typealias DeletionCompletion = (Error?) -> Void
+
+	// MARK: Internal
+
 	var deleteCachedFeedCallCount = 0
 	var insertCallCount = 0
-	
-	typealias DeletionCompletion = (Error?) -> Void
-	
-	private var deletionCompletions = [DeletionCompletion]()
-	
+	var insertions = [(items: [FeedItem], timestamp: Date)]()
+
 	func deleteCacheFeed(completion: @escaping DeletionCompletion) {
 		deleteCachedFeedCallCount += 1
 		deletionCompletions.append(completion)
 	}
-	
+
 	func completeDeletion(with error: Error, at index: Int = 0) {
 		deletionCompletions[index](error)
 	}
-	
+
 	func completeDeletionSuccessfully(at index: Int = 0) {
 		deletionCompletions[index](nil)
 	}
-	
-	func insert(_ items: [FeedItem]) {
+
+	func insert(_ items: [FeedItem], timestamp: Date) {
 		insertCallCount += 1
+		insertions.append((items, timestamp))
 	}
+
+	// MARK: Private
+
+	private var deletionCompletions = [DeletionCompletion]()
 }
 
+// MARK: - CacheFeedUseCaseTests
+
 final class CacheFeedUseCaseTests: XCTestCase {
-    func test_init_doesNotDeleteCacheUponCreation() {
+	// MARK: Internal
+
+	func test_init_doesNotDeleteCacheUponCreation() {
 		let (_, store) = makeSUT()
-		_ = LocalFeedLoader(store: store)
-		
+
 		XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
-    }
-	
+	}
+
 	func test_save_requestsCacheDeletion() {
 		let items = [uniqueItem(), uniqueItem()]
 		let (sut, store) = makeSUT()
-		
+
 		sut.save(items)
-		
+
 		XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
 	}
-	
+
 	func test_save_deosNotRequestCacheInsertionOnDeletionError() {
 		let items = [uniqueItem(), uniqueItem()]
 		let (sut, store) = makeSUT()
 		let deletionError = anyNSError()
-		
+
 		sut.save(items)
 		store.completeDeletion(with: deletionError)
-		
+
 		XCTAssertEqual(store.insertCallCount, 0)
 	}
-	
+
 	func test_save_requestsNewCacheInsertionOnSuccessfulDeletion() {
 		let items = [uniqueItem(), uniqueItem()]
 		let (sut, store) = makeSUT()
-		
+
 		sut.save(items)
 		store.completeDeletionSuccessfully()
-		
+
 		XCTAssertEqual(store.insertCallCount, 1)
 	}
-	
+
+	func test_save_requestsNewCacheInsertionWithTimestampOnSuccessfulDeletion() {
+		let timestamp = Date()
+		let items = [uniqueItem(), uniqueItem()]
+		let (sut, store) = makeSUT(currentDate: { timestamp })
+
+		sut.save(items)
+		store.completeDeletionSuccessfully()
+
+		XCTAssertEqual(store.insertions.count, 1)
+		XCTAssertEqual(store.insertions.first?.items, items)
+		XCTAssertEqual(store.insertions.first?.timestamp, timestamp)
+	}
+
+	// MARK: Private
+
 	// MARK: - Helpers
-	
-	private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
+
+	private func makeSUT(
+		currentDate: @escaping () -> Date = Date.init,
+		file: StaticString = #filePath,
+		line: UInt = #line
+	) -> (sut: LocalFeedLoader, store: FeedStore) {
 		let store = FeedStore()
-		let sut = LocalFeedLoader(store: store)
+		let sut = LocalFeedLoader(store: store, currentDate: currentDate)
 		trackForMemoryLeaks(store, file: file, line: line)
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return (sut, store)
 	}
-	
+
 	private func uniqueItem() -> FeedItem {
 		return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
 	}
-	
+
 	private func anyURL() -> URL {
 		return URL(string: "http://any-url.com")!
 	}
-	
+
 	private func anyNSError() -> NSError {
 		return NSError(domain: "any error", code: 0)
 	}
