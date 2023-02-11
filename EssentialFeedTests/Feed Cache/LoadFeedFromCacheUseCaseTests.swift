c009cde68aa208a0488b62c84d560f83578f60a8
Federico Arvat <arvat.federico@gmail.com>
2023-02-11 16:00:35 +0100

    localFeedLoader does not message store upon creation before loading the feed from the cache store

---
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -0,0 +1,77 @@
+//
+//  LoadFeedFromCacheUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Federico Arvat on 11/02/23.
+//
+
+import XCTest
+import EssentialFeed
+
+class LoadFeedFromCacheUseCaseTests: XCTestCase {
+	
+	func test_init_doesNotMessageStoreUponCreation() {
+		let (_, store) = makeSUT()
+		
+		XCTAssertEqual(store.receivedMessages, [])
+	}
+	
+	private func makeSUT(
+		currentDate: @escaping () -> Date = Date.init,
+		file: StaticString = #filePath,
+		line: UInt = #line
+	) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+		let store = FeedStoreSpy()
+		let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+		trackForMemoryLeaks(store, file: file, line: line)
+		trackForMemoryLeaks(sut, file: file, line: line)
+		return (sut, store)
+	}
+	
+	private class FeedStoreSpy: FeedStore {
+		typealias DeletionCompletion = (Error?) -> Void
+		typealias InsertionCompletion = (Error?) -> Void
+		
+		// MARK: Internal
+		
+		enum ReceivedMessage: Equatable {
+			case deleteCachedFeed
+			case insert([LocalFeedImage], Date)
+		}
+		
+		private(set) var receivedMessages = [ReceivedMessage]()
+		
+		func deleteCacheFeed(completion: @escaping DeletionCompletion) {
+			deletionCompletions.append(completion)
+			receivedMessages.append(.deleteCachedFeed)
+		}
+		
+		func completeDeletion(with error: Error, at index: Int = 0) {
+			deletionCompletions[index](error)
+		}
+		
+		func completeDeletionSuccessfully(at index: Int = 0) {
+			deletionCompletions[index](nil)
+		}
+		
+		func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+			insertionCompletions.append(completion)
+			receivedMessages.append(.insert(feed, timestamp))
+		}
+		
+		func completeInsertion(with error: Error, at index: Int = 0) {
+			insertionCompletions[index](error)
+		}
+		
+		func completeInsertionSuccessfully(at index: Int = 0) {
+			insertionCompletions[index](nil)
+		}
+		
+		// MARK: Private
+		
+		private var deletionCompletions = [DeletionCompletion]()
+		
+		private var insertionCompletions = [InsertionCompletion]()
+	}
+	
+}
