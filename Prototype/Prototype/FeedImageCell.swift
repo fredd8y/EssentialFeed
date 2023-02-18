c922dc62f421c4d52f5384ad5bc016a93b8caec5
Federico Arvat <arvat.federico@gmail.com>
2023-02-18 17:34:37 +0100

    added FeedImageCell class

---
+++ Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by Federico Arvat on 17/02/23.
+//
+
+import UIKit
+
+final class FeedImageCell: UITableViewCell {
+	@IBOutlet private(set) var locationContainer: UIView!
+	@IBOutlet private(set) var locationLabel: UILabel!
+	@IBOutlet private(set) var feedImageView: UIImageView!
+	@IBOutlet private(set) var descriptionLabel: UILabel!
+}
