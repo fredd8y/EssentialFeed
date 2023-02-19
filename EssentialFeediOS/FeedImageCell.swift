305a5151bc432c052878594fdcdd81d087f21d0d
Federico Arvat <arvat.federico@gmail.com>
2023-02-19 16:04:38 +0100

    render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Federico Arvat on 19/02/23.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+	
+	public let locationContainer = UIView()
+	public let locationLabel = UILabel()
+	public let descriptionLabel = UILabel()
+	
+}
