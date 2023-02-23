4d9e23d143ad6306cc045108977b960b97666b9e
Federico Arvat <arvat.federico@gmail.com>
2023-02-23 21:29:06 +0100

    added Decorator to dispatch to MainQueue, this decorator is used in the Composer layer to not leak implementation detail in the client's file

---
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -0,0 +1,43 @@
+//
+//  FeedUIComposer.swift
+//  EssentialFeediOS
+//
+//  Created by Federico Arvat on 21/02/23.
+//
+
+import EssentialFeed
+import UIKit
+
+// MARK: - FeedUIComposer
+
+public final class FeedUIComposer {
+	// MARK: Lifecycle
+
+	private init() {}
+
+	// MARK: Public
+
+	public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
+		let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader:
+																	MainQueueDispatchDecorator(decoratee: feedLoader))
+		
+		let feedController = makeFeedViewController(delegate: presentationAdapter, title: FeedPresenter.title)
+		
+		presentationAdapter.presenter = FeedPresenter(
+			feedView: FeedViewAdapter(
+				controller: feedController,
+				imageLoader: MainQueueDispatchDecorator(decoratee: imageLoader)),
+			loadingView: WeakRefVirtualProxy(feedController))
+		
+		return feedController
+	}
+	
+	private static func makeFeedViewController(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
+		let bundle = Bundle(for: FeedViewController.self)
+		let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+		let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
+		feedController.delegate = delegate
+		feedController.title = title
+		return feedController
+	}
+}
