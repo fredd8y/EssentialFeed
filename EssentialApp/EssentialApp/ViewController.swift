d0e79293614b5f8e9315df69213d775860cb140a
Federico Arvat <arvat.federico@gmail.com>
2023-02-27 19:14:15 +0100

    Added empty EssentialApp project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Federico Arvat on 27/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
