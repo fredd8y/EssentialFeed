bdc46574eb6e1911808d9cf9b5e8fc053856b8b4
Federico Arvat <federico.arvat@certimetergroup.com>
2023-03-11 16:16:54 +0100

    Removed main storyboard, fixed import issues for EssentialAppTests

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -14,7 +14,6 @@ import EssentialFeedCache
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	var window: UIWindow?
 	
-	
 	private lazy var httpClient: HTTPClient = {
 		URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
 	}()
@@ -37,7 +36,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	}
 	
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-		guard let _ = (scene as? UIWindowScene) else { return }
+		guard let scene = (scene as? UIWindowScene) else { return }
+		
+		window = UIWindow(windowScene: scene)
 		
 		configureWindow()
 	}
@@ -62,6 +63,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 					fallback: FeedImageDataLoaderCacheDecorator(
 						decoratee: remoteImageLoader,
 						cache: localImageLoader))))
+		
+		window?.makeKeyAndVisible()
 	}
 	
 	func sceneWillResignActive(_ scene: UIScene) {
--- EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+Localization.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+Localization.swift
@@ -5,6 +5,7 @@
 import Foundation
 import XCTest
 import EssentialFeed
+import EssentialFeedPresentation
 
 extension FeedUIIntegrationTests {
 	func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
