5b1bd12ea260283e07091f031201be0bf1ece05b
Federico Arvat <arvat.federico@gmail.com>
2023-07-04 12:22:06 +0200

    Rename and move ResourceLoadingView and VIewModel to Shared module

---
+++ EssentialFeed/Shared Presentation/ResourceLoadingView.swift
@@ -0,0 +1,13 @@
+//
+//  ResourceLoadingView.swift
+//  EssentialFeed
+//
+//  Created by Federico Arvat on 04/07/23.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public protocol ResourceLoadingView {
+	func display(_ viewModel: ResourceLoadingViewModel)
+}
