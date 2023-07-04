d01632918c64eaeb6ea4d7a7575398e714fc0da2
Federico Arvat <arvat.federico@gmail.com>
2023-07-04 18:41:02 +0200

    Duplicate FeedUIComposer as CommentsUIComposer

--- EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -8,8 +8,8 @@ import EssentialApp
 import EssentialFeed
 import EssentialFeediOS
 
-final class FeedUIIntegrationTests: XCTestCase {
-	
+class FeedUIIntegrationTests: XCTestCase {
+
 	func test_feedView_hasTitle() {
 		let (sut, _) = makeSUT()
 		
